3eeff43a52187f0c5638d347d0d9eb798b01e072
Kyle <kyle@citrine.io>
2016-02-09 10:30:50 -0800

    ideal + actual weight and atomic percentages in composition

--- pypif/obj/system/chemical/common/composition.py
+++ pypif/obj/system/chemical/common/composition.py
@@ -1,4 +1,5 @@
 from pypif.obj.common.pio import Pio
+from pypif.obj.common.scalar import Scalar
 
 
 class Composition(Pio):
@@ -6,16 +7,81 @@ class Composition(Pio):
     Class to store information about an element in a composition vector using weight percents or atomic percents.
     """
 
-    def __init__(self, element=None, weight_percent=None, atomic_percent=None, **kwargs):
+    def __init__(self, element=None, actual_weight_percent=None, actual_atomic_percent=None, ideal_weight_percent=None,
+                 ideal_atomic_percent=None, **kwargs):
         """
         Constructor.
 
         :param element: Symbol of the element.
-        :param weight_percent: Percentage of the weight of the material that is this element.
-        :param atomic_percent: Percentage of the atoms in the material that are this element.
+        :param actual_weight_percent: Actual percentage of the weight of the chemical that is this element
+        - :class:`.Scalar` object.
+        :param actual_atomic_percent: Actual percentage of the atoms in the chemical that are this element
+        - :class:`.Scalar` object.
+        :param ideal_weight_percent: Ideal percentage of the weight of the chemical that is this element
+        - :class:`.Scalar` object.
+        :param ideal_atomic_percent: Ideal percentage of the atoms in the chemical that are this element
+        - :class:`.Scalar` object.
         :param kwargs: Dictionary of field names not supported.
         """
         super(Composition, self).__init__(**kwargs)
+        
+        # These are the members that have explicit getters and setters
+        self._actual_weight_percent = None
+        self._actual_atomic_percent = None
+        self._ideal_weight_percent = None
+        self._ideal_atomic_percent = None
+        
+        # Set the values for this object
         self.element = element
-        self.weight_percent = weight_percent
-        self.atomic_percent = atomic_percent
+        self.actual_weight_percent = actual_weight_percent
+        self.actual_atomic_percent = actual_atomic_percent
+        self.ideal_weight_percent = ideal_weight_percent
+        self.ideal_atomic_percent = ideal_atomic_percent
+        
+    @property
+    def actual_weight_percent(self):
+        return self._actual_weight_percent
+        
+    @actual_weight_percent.setter
+    def actual_weight_percent(self, value):
+        self._actual_weight_percent = self._get_object(Scalar, value)
+    
+    @actual_weight_percent.deleter
+    def actual_weight_percent(self):
+        del self._actual_weight_percent
+    
+    @property
+    def actual_atomic_percent(self):
+        return self._actual_atomic_percent
+        
+    @actual_atomic_percent.setter
+    def actual_atomic_percent(self, value):
+        self._actual_atomic_percent = self._get_object(Scalar, value)
+    
+    @actual_atomic_percent.deleter
+    def actual_atomic_percent(self):
+        del self._actual_atomic_percent
+    
+    @property
+    def ideal_weight_percent(self):
+        return self._ideal_weight_percent
+        
+    @ideal_weight_percent.setter
+    def ideal_weight_percent(self, value):
+        self._ideal_weight_percent = self._get_object(Scalar, value)
+    
+    @ideal_weight_percent.deleter
+    def ideal_weight_percent(self):
+        del self._ideal_weight_percent
+    
+    @property
+    def ideal_atomic_percent(self):
+        return self._ideal_atomic_percent
+        
+    @ideal_atomic_percent.setter
+    def ideal_atomic_percent(self, value):
+        self._ideal_atomic_percent = self._get_object(Scalar, value)
+    
+    @ideal_atomic_percent.deleter
+    def ideal_atomic_percent(self):
+        del self._ideal_atomic_percent
