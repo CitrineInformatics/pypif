cc22b97edc121f15174b70f7836033171572e0c1
Kyle <kyle@citrine.io>
2016-03-23 12:45:26 -0700

    fill out scalar class

--- pypif/obj/common/scalar.py
+++ pypif/obj/common/scalar.py
@@ -7,18 +7,25 @@ class Scalar(Pio):
     a range of values, a minimum, or a maximum.
     """
 
-    def __init__(self, value=None, minimum=None, maximum=None, uncertainty=None, **kwargs):
+    def __init__(self, value=None, minimum=None, inclusiveMinimum=None, maximum=None, inclusiveMaximum=None,
+                 uncertainty=None, approximate=None, **kwargs):
         """
         Constructor.
 
         :param value: Exact value for the point.
         :param minimum: Minimum value for the point.
+        :apram inclusiveMinimum: Whether the minimum is inclusive.
         :param maximum: Maximum value for the point.
+        :param inclusiveMaximum: Whether the maximum is inclusive.
         :param uncertainty: Isotropic uncertainty for the point.
+        :param approximate: Whether the value is approximate.
         :param kwargs: Dictionary of field names not supported.
         """
         super(Scalar, self).__init__(**kwargs)
         self.value = value
         self.minimum = minimum
+        self.inclusiveMinimum = inclusiveMinimum
         self.maximum = maximum
+        self.inclusiveMaximum = inclusiveMaximum
         self.uncertainty = uncertainty
+        self.approximate = approximate
