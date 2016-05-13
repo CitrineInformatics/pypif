5fd0d471c9ed42758659801fc8974040b7191ebd
Kyle <kyle@citrine.io>
2016-05-13 14:16:51 -0700

    add instruments and software to process_step

--- pypif/obj/common/process_step.py
+++ pypif/obj/common/process_step.py
@@ -1,5 +1,7 @@
 from pypif.obj.common.pio import Pio
 from pypif.obj.common.value import Value
+from pypif.obj.common.software import Software
+from pypif.obj.common.instrument import Instrument
 
 
 class ProcessStep(Pio):
@@ -7,12 +9,14 @@ class ProcessStep(Pio):
     Information about a single step in a processing pipeline.
     """
 
-    def __init__(self, name=None, details=None, **kwargs):
+    def __init__(self, name=None, details=None, instruments=None, software=None, **kwargs):
         """
         Constructor.
 
         :param name: String with the name of the process step.
         :param details: List of dictionaries or :class:`.Value` objects with the details of the step.
+        :param instruments: List of dictionaries or :class:`.Instrument` objects.
+        :param software: List of dictionaries or :class:`.Software` objects.
         :param kwargs: Dictionary of fields that are not supported.
         """
         super(ProcessStep, self).__init__(**kwargs)
@@ -20,6 +24,10 @@ class ProcessStep(Pio):
         self.name = name
         self._details = None
         self.details = details
+        self._instruments = None
+        self.instruments = instruments
+        self._software = None
+        self.software = software
 
     @property
     def name(self):
@@ -46,3 +54,25 @@ class ProcessStep(Pio):
     @details.deleter
     def details(self):
         self._details = None
+
+    @property
+    def instruments(self):
+        return self._instruments
+
+    @instruments.setter
+    def instruments(self, instruments):
+        self._validate_list_type('instruments', instruments, dict, Instrument)
+        self._instruments = self._get_object(Instrument, instruments)
+
+    @instruments.deleter
+    def instruments(self):
+        self._instruments = None
+
+    @property
+    def software(self):
+        return self._software
+
+    @software.setter
+    def software(self, software):
+        self._validate_list_type('software', software, dict, Software)
+        self._software = self._get_object(Software, software)
