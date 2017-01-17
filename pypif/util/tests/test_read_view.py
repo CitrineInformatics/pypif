df72b7d3c9dab6d2885cf8e15230e22a30ccd73f
Max Hutchinson <maxhutch@gmail.com>
2017-01-17 07:03:55 -0600

    Clean up

--- pypif/util/tests/test_read_view.py
+++ pypif/util/tests/test_read_view.py
@@ -1,7 +1,9 @@
 from pypif.obj import System, Property
 from pypif.util.read_view import ReadView
 
+
 def test_read_view():
+    """Test that properties are passed through to the readview"""
     pif = System()
     pif.uid = "10245"
     pif.names = ["example", "ex"]
@@ -12,18 +14,40 @@ def test_read_view():
     assert r.properties["foo"].scalars == 1.0
     assert r.properties["bar"].scalars == 2.0
 
+
+def test_unambig():
+    """Test that properties are mirrored in a top level dic"""
+    pif = System()
+    pif.properties = [Property(name="foo", scalars=1.0), Property(name="bar", scalars=2.0)]
+    r = ReadView(pif)
+    assert r["foo"].scalars == 1.0
+    assert r["bar"].scalars == 2.0
+
+
 def test_nested_read_view():
+    """Test that nested Pios (system here) are recursively processed"""
     pif = System()
     pif.uid = "10245"
-    pif.names = ["example", "ex"]
     pif.properties = [Property(name="foo", scalars=1.0), Property(name="bar", scalars=2.0)]
-    pif2 = System(sub_systems = [pif,])
+    pif2 = System(sub_systems=[pif])
     r = ReadView(pif2)
     assert r.sub_systems["10245"].uid == pif.uid
-    assert r.sub_systems["10245"].names == pif.names
+    assert r["10245"].uid == pif.uid
     assert r.sub_systems["10245"].properties["foo"].scalars == 1.0
     assert r.sub_systems["10245"].properties["bar"].scalars == 2.0
     assert r["foo"].scalars == 1.0
     assert r["bar"].scalars == 2.0
 
 
+def test_ambiguity():
+    """Test that ambiguous keys are removed from the top level dict"""
+    pif = System()
+    pif.uid = "10245"
+    pif.properties = [Property(name="foo", scalars=1.0), Property(name="bar", scalars=2.0)]
+    pif2 = System(sub_systems = [pif,], properties=[Property(name="foo", scalars=10.0)])
+    r = ReadView(pif2)
+    assert r.properties["foo"].scalars == 10.0
+    assert r.sub_systems["10245"].properties["foo"].scalars == 1.0
+    assert "foo" not in r.keys()
+    assert r.sub_systems["10245"]["foo"].scalars == 1.0
+    assert r["bar"].scalars == 2.0
