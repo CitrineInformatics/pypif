c87a7bc10e897b6e4173920104e6db7c80024ce4
Ryan Latture <ryan.latture@gmail.com>
2018-10-31 07:45:26 -0700

    Version bump.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pypif',
-      version='2.1.0',
+      version='2.1.1',
       url='http://github.com/CitrineInformatics/pypif',
       description='Python tools for working with the Physical Information File (PIF)',
       author='Kyle Michel',
