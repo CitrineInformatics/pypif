c62bc0ab10498d76c3109aa815fb6cf86a532243
Max Hutchinson <maxhutch@gmail.com>
2020-04-30 21:51:56 -0400

    Version bump and update author

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='pypif',
-      version='2.1.1',
+      version='2.1.2',
       url='http://github.com/CitrineInformatics/pypif',
       description='Python tools for working with the Physical Information File (PIF)',
-      author='Kyle Michel',
-      author_email='kyle@citrine.io',
+      author='Citrine Informatics',
       packages=find_packages(),
       install_requires=[
             'six>=1.10.0,<2',
